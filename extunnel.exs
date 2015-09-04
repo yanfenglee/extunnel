7fac50de1fbe00034656366db64b819b8a8f2c71
liyanfeng <lyf@a.com>
2015-09-04 14:48:45 +0800

    fix socket close

--- extunnel.exs
+++ extunnel.exs
@@ -6,28 +6,29 @@ defmodule Extunnel do
     @secret "extunnel"
 
     def start do
-      {:ok, socket} = :gen_tcp.listen(@listen,[:binary, active: false, reuseaddr: true])
-      IO.puts "Accepting connections on port #{@listen}"
-      loop_acceptor(socket)
+        {:ok, socket} = :gen_tcp.listen(@listen,[:binary, active: false, reuseaddr: true])
+        IO.puts "Accepting connections on port #{@listen}"
+        loop_acceptor(socket)
     end
 
     defp loop_acceptor(socket) do
-      {:ok, client} = :gen_tcp.accept(socket)
-      state = :crypto.stream_init(:rc4, @secret)
-      {ip,port} = @backend
-      spawn fn ->
-          IO.puts "begin connect backend..."
-          {:ok, backend} = :gen_tcp.connect(ip, port, [:binary, active: false])
-          IO.puts "begin spawn pump..."
-          spawn fn -> pump(client, backend, state) end
-          spawn fn -> pump(backend, client, state) end
+        {:ok, frontend} = :gen_tcp.accept(socket)
+        state = :crypto.stream_init(:rc4, @secret)
+        {ip,port} = @backend
 
-          receive do
-              {:msg,contents} ->
-          end
-      end
+        spawn fn ->
+            Process.flag(:trap_exit, true)
 
-      loop_acceptor(socket)
+            {:ok, backend} = :gen_tcp.connect(ip, port, [:binary, active: false])
+            spawn_link fn -> pump(frontend, backend, state) end
+            spawn_link fn -> pump(backend, frontend, state) end
+
+            receive do
+                {:EXIT,from,reason} -> :gen_tcp.close(frontend);
+            end
+        end
+
+        loop_acceptor(socket)
     end
 
     defp pump(s1, s2, state) do
@@ -44,17 +45,12 @@ defmodule Extunnel do
     end
 
     defp read(socket) do
-        IO.puts "begin receive"
-        {ret, data} = :gen_tcp.recv(socket, 0)
-        IO.puts "receive: #{data}"
+        {:ok, data} = :gen_tcp.recv(socket, 0)
         data
     end
 
     defp write({state, data}, socket) do
-        IO.puts "begin send"
         :ok = :gen_tcp.send(socket, data)
-        IO.puts "send ok..."
-
         state
     end
 end
